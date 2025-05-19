934fdd09326621350000dd05b1b428f4a4c8e88d
Bardasson <121575366+bardasson10@users.noreply.github.com>
2025-05-19 15:54:04 -0300

    #Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456789", "Modelo Nokia", "123456789012345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone: ");
+Smartphone iphone = new Iphone("987654321", "Modelo Iphone", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
